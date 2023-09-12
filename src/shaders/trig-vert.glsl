0e2311c4deeb2df5e5d3f8a0c37ec40808c84626
kevinz2003 <62074534+kevinz2003@users.noreply.github.com>
2023-09-11 22:20:36 -0400

    push

--- src/shaders/trig-vert.glsl
+++ src/shaders/trig-vert.glsl
@@ -35,7 +35,7 @@ out vec4 fs_Pos;
 
 const vec4 lightPos = vec4(5, 5, 3, 1); //The position of our virtual light, which is used to compute the shading of
                                         //the geometry in the fragment shader.
-
+ 
 void main()
 {
     fs_Col = vs_Col;                         // Pass the vertex colors to the fragment shader for interpolation
